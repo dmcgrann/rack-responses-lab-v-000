bf5dfea6732ddccc810479568849b3ef8ac1fd64
David McGrann <dpmcgrann@gmail.com>
2018-07-21 18:19:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
+    hour = Kernel.rand(1..20)
+    
+    
+    
     if DateTime.now.hour <= 12
       resp.write "Good Morning!"
     elsif DateTime.now.hour >= 12
