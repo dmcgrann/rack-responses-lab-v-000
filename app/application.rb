6ee6fe3d4cd962834b79f2485a84740c03a3dc67
David McGrann <dpmcgrann@gmail.com>
2018-07-21 18:22:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,13 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    hour = Kernel.rand(1..18)
+    hour = Kernel.rand(1..15)
     
     resp.write "#{hour}\n"
     
     if hour <= 12
       resp.write "Good Morning!"
-    elsif hour >= 12 && hour <= 18
+    elsif hour >= 12 && hour <= 15
       resp.write "Good Afternoon!"
     end
     resp.finish
